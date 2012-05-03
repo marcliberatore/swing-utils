ca7200c3e04e28e792b2bd50d6ca476601fa81dc
Shigeru Fujiwara <sgr0502@gmail.com>
2012-05-03 14:21:32 +0900

    init

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject swing-utils "0.1.0"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/swing_utils/test/core.clj
@@ -0,0 +1,6 @@
+(ns swing-utils.test.core
+  (:use [swing-utils.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
