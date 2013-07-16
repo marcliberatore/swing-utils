3e5cfd2eae50b0f7b2d87e229149ca55272fe2af
Marc Liberatore <liberato@cs.umass.edu>
2013-07-16 08:23:34 -0400

    add license information

--- project.clj
+++ project.clj
@@ -2,3 +2,5 @@
   :dependencies [[org.clojure/clojure "[1.3.0,)"]]
   :main clojure.tools.swing-utils)
   :description "useful functions for interfacing Clojure to Swing"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
