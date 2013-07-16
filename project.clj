2729558b437155b9ff0c618aeacf58f06cd82834
Marc Liberatore <liberato@cs.umass.edu>
2013-07-16 08:24:40 -0400

    use fixed clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject swing-utils "0.2.0"
-  :dependencies [[org.clojure/clojure "[1.3.0,)"]]
   :description "useful functions for interfacing Clojure to Swing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
