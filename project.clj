5b45b6f8ac36504162589cb5dcfde6b9604cf107
Marc Liberatore <liberato@cs.umass.edu>
2013-07-16 08:30:06 -0400

    add :url

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "useful functions for interfacing Clojure to Swing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/marcliberatore/swing-utils"
   :dependencies [[org.clojure/clojure "1.5.1"]])
