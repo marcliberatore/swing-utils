d21190c18500089782c4426b9cf7b73bd4ea1067
Marc Liberatore <liberato@cs.umass.edu>
2013-07-16 08:24:03 -0400

    no -main means no :main

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject swing-utils "0.2.0"
   :dependencies [[org.clojure/clojure "[1.3.0,)"]]
-  :main clojure.tools.swing-utils)
   :description "useful functions for interfacing Clojure to Swing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
